213c63b87ce7ae04ef3720a5343d916b5075d07f
giorgio <giorgio@polisoftware.com.br>
2023-04-11 09:49:47 -0300

    - Update for horse 3.1.0

--- src/Horse.Upload.pas
+++ src/Horse.Upload.pas
@@ -203,15 +203,11 @@ var
   LContent: TObject;
   LUploadConfg : TUploadConfig;
 begin
-  //LWebRequest := THorseHackRequest(Req).GetWebRequest;
   LWebRequest := Req.RawWebRequest;
 
   Next;
 
-  //LWebResponse := THorseHackResponse(Res).GetWebResponse;
   LWebResponse := Res.RawWebResponse;
-
-  //LContent := THorseHackResponse(Res).GetContent;
   LContent := Res.Content;
 
   if Assigned(LContent) and LContent.InheritsFrom(TUploadConfig) then
